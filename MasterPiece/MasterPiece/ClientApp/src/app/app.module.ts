00c752e4f23db1ea9294e63bf944842dff765f27
Marco Grison <grisonmarco2020@gmail.com>
2021-04-07 21:01:59 -0300

    Criação de telas Login e Cadastro

--- MasterPiece/MasterPiece/ClientApp/src/app/app.module.ts
+++ MasterPiece/MasterPiece/ClientApp/src/app/app.module.ts
@@ -9,14 +9,17 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import {LoginScreenComponent} from './login-screen/login-screen.component'
+import {RegisterScreenComponent} from './register-screen/register-screen.component'
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    LoginScreenComponent,
+    RegisterScreenComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +29,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'login-screen', component: LoginScreenComponent },
+      { path: 'register-screen', component: RegisterScreenComponent},
     ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
