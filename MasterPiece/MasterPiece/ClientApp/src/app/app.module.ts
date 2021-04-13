881eb474826077dc2b668a3f0b9f291ce13efc72
marcogrisonsige <marco.grison@sigeatende.com.br>
2021-04-13 13:10:58 -0300

    Changes

--- MasterPiece/MasterPiece/ClientApp/src/app/app.module.ts
+++ MasterPiece/MasterPiece/ClientApp/src/app/app.module.ts
@@ -5,19 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import {LoginScreenComponent} from './login-screen/login-screen.component'
 import {RegisterScreenComponent} from './register-screen/register-screen.component'
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     LoginScreenComponent,
     RegisterScreenComponent
   ],
@@ -27,8 +21,6 @@ import {RegisterScreenComponent} from './register-screen/register-screen.compone
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'login-screen', component: LoginScreenComponent },
       { path: 'register-screen', component: RegisterScreenComponent},
     ])
