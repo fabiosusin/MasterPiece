5b2c6f04c11064557ef601297b8deccd6677836b
Marco Grison <grisonmarco2020@gmail.com>
2021-05-12 00:18:45 -0300

    ajuste de merda

---
+++ Business/Logic/Products/BlProductsList.cs
@@ -0,0 +1,36 @@
+﻿using DAO.Databases;
+using DAO.Input;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using Repository.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Logic.Products
+{
+    public class BlProductsList : BlAbstract<Product>
+    {
+        public BlProductsList(IMasterPieceDatabaseSettings settings) : base(settings) { }
+
+        private IMongoQuery QueryFilters(FiltersProducts filters)
+        {
+            var query = new List<IMongoQuery>();
+            if (!string.IsNullOrEmpty(filters.Category))
+                query.Add(Query<Product>.EQ(x => x.Category, filters.Category));
+
+            if (!string.IsNullOrEmpty(filters.Price.ToString()))
+                query.Add(Query<Product>.EQ(x => x.TotalValue, filters.Price));
+
+            if (!string.IsNullOrEmpty(filters.ProductName))
+                query.Add(Query<Product>.EQ(x => x.Name, filters.ProductName));
+
+
+            return Query.And(query);
+        }
+
+        public List<Product> GetProducts(FiltersProducts filters) => Collection.Find(QueryFilters(filters)).ToList();
+    }
+}
--- MasterPiece/Controllers/ProductsListController.cs
+++ MasterPiece/Controllers/ProductsListController.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using Business.Logic.Products;
+using DAO.Input;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Repository.Settings;
+using System;
+using System.Collections.ObjectModel;
 
 namespace MasterPiece.Controllers
 {
     public class ProductsListController
     {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ProductsRegisterController : ControllerBase
+        {
+            private readonly BlProductsList _blProductsList;
+            public ProductsRegisterController(IMasterPieceDatabaseSettings settings)
+            {
+                _blProductsList = new BlProductsList(settings);
+            }
+
+            [HttpPost, Route("List")]
+            public IActionResult List([FromBody] FiltersProducts filtersproduct)
+            {
+                var products = _blProductsList.GetProducts(filtersproduct);
+                return Ok(products);
+            }
+        }
 
     }
 }
